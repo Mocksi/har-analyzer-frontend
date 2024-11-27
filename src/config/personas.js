56cc9f07c7c2759716d5f6e1658f59ebcc7fd635
drewtang <dillonan@gmail.com>
2024-11-27 17:52:42 -0600

    build errors

---
+++ src/config/personas.js
@@ -0,0 +1,20 @@
+export const PERSONA_CONFIGS = {
+  developer: {
+    metrics: ['responseTime', 'bundleSize', 'codeOptimization'],
+    insights: ['technical', 'performance'],
+    charts: ['timeseriesDetailed', 'resourceBreakdown'],
+    actionItems: ['codeLevel', 'optimization']
+  },
+  qa: {
+    metrics: ['errorRates', 'userFlows', 'browserCompatibility'],
+    insights: ['testing', 'reliability'],
+    charts: ['errorPatterns', 'userJourneys'],
+    actionItems: ['testCases', 'regressionPoints']
+  },
+  salesEngineer: {
+    metrics: ['businessImpact', 'userExperience', 'competitiveEdge'],
+    insights: ['business', 'customer'],
+    charts: ['highLevel', 'comparison'],
+    actionItems: ['roi', 'customerValue']
+  }
+}; 
